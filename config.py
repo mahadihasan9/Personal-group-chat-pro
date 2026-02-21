e4e5b17983049ad8a8fd4832e31ea25a418e22f4
Mahdi bin Iqbal <151855877+mahadihasan9@users.noreply.github.com>
2026-02-21 23:01:18 +0600

    Update site password and clean up comments

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # --- config.py ---
 
 import os
-# প্রজেক্টের রুট ডিরেক্টরি
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY = 'YOUR_SUPER_SECRET_KEY_HERE' 
-SITE_PASSWORD = "u"  
+SITE_PASSWORD = "admin"  
 
 # JSON Data file Configuration
-# FIX: JSON ফাইলটি static ফোল্ডারের ভেতরে রাখা হলো
+
 STATIC_FOLDER = os.path.join(BASE_DIR, 'static')
 JSON_DATA_FILE_NAME = 'chat_data.json'
 UPLOAD_FOLDER_NAME = 'uploads' # static/uploads
